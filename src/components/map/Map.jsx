59e0d26b0b1cc13d9effd7c5eb51e50b7a318185
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-12 16:02:01 +0300

    current map was customized

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,11 +1,25 @@
-import { YMaps, Map as YMapsMap } from '@pbe/react-yandex-maps';
+import { YMaps, Map as YMapsMap , Placemark} from '@pbe/react-yandex-maps';
 
 export const Map = () => {
     return (
         <>
             <YMaps>
                 <div>
-                    <YMapsMap defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+                    <YMapsMap defaultState={{
+                        center: [55.75, 37.57],
+                        zoom: 9,
+                        controls: ["zoomControl", "fullscreenControl"]
+                    }} modules={["control.ZoomControl", "control.FullscreenControl"]}
+                    >
+                        <Placemark
+                            modules={["geoObject.addon.balloon"]}
+                            defaultGeometry={[55.751574, 37.573856]}
+                            properties={{
+                                balloonContentBody:
+                                    "This is balloon loaded by the Yandex.Maps API module system",
+                            }}
+                        />
+                    </YMapsMap>
                 </div>
             </YMaps>
         </>
