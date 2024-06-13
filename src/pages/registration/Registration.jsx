723b2a4147d0a16828e575371fb96661353578f1
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-13 20:25:34 +0300

    Registration page layout was made

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,9 +1,52 @@
 import './Registration.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export const Registration = () => {
     return (
-      <>
-        <h1>Registration</h1>
-      </>
+        <>
+            <div className="registration-container">
+                <h1>Регистрация</h1>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': {m: 1, width: '25ch'},
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div>
+                        <TextField
+                            id="outlined-error"
+                            label="Логин"
+                            placeholder="Логин"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            id="outlined-error"
+                            label="Почта"
+                            placeholder="Почта"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            id="outlined-error"
+                            label="Пароль"
+                            placeholder="Пароль"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            id="outlined-error"
+                            label="Повторите пароль"
+                            placeholder="Повторите пароль"
+                        />
+                    </div>
+                    <Button variant="contained">Зарегистрироваться</Button>
+                </Box>
+            </div>
+        </>
     );
 }
